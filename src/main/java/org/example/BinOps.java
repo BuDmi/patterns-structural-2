26ccaf7084578097e029852db8604c8a2f0a5db2
Dmitriy Bubyr <d.bubyr@lancktele.com>
2022-08-24 09:57:08 +0300

    Fix

--- src/main/java/org/example/BinOps.java
+++ src/main/java/org/example/BinOps.java
@@ -1,6 +1,5 @@
 package org.example;
 
-// Для парсинга из двоичной записи используйте Integer.parseInt(binStr, 2), для конвертации обратно - Integer.toBinaryString.
 public class BinOps {
     public String sum(String a, String b) {
         int sum = Integer.parseInt(a, 2) + Integer.parseInt(b, 2);
